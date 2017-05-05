c91523295794eac0372ed03e4c8c00241a114657
Takahiro Suzuki <bur8787@gmail.com>
2017-05-05 20:50:14 +0900

    AWS Lambdaで使用するため、入出力をオブジェクト型に変更

--- api/src/main/java/jp/suzutt/sample/lambda/SampleLambdaHandler.java
+++ api/src/main/java/jp/suzutt/sample/lambda/SampleLambdaHandler.java
@@ -1,11 +1,18 @@
 package jp.suzutt.sample.lambda;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class SampleLambdaHandler implements RequestHandler<Object, String> {
+public class SampleLambdaHandler implements RequestHandler<Object, Object> {
 
-	public String handleRequest(Object request, Context contexst) {
-		return "Hello,suzutt";
+	public Object handleRequest(Object request, Context contexst) {
+		Map<String,String> map = new HashMap<String, String>();
+		String key = "name";
+		String value = "suzutt";
+		map.put(key, value);
+		return map;
 	}
 }
