c822f6a3e3cafb689c1320e7fc070ca4f4c7b1aa
Takahiro Suzuki <bur8787@gmail.com>
2017-05-14 14:06:12 +0900

    gradleマルチプロジェクト化

---
+++ api/src/main/java/jp/suzutt/api/ProfileRequestHandler.java
@@ -0,0 +1,19 @@
+package jp.suzutt.api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class ProfileRequestHandler implements RequestHandler<Object, Object> {
+
+	public Object handleRequest(Object request, Context contexst) {
+		Map<String,String> map = new HashMap<String, String>();
+		String key = "name";
+		String value = "suzutt";
+		map.put(key, value);
+		return map;
+	}
+	
+}
